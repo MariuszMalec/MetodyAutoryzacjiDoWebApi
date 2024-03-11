1654753096b9ad0252a35a00785cc2ed9dac40ef
MariuszMalec <mario106c@gmail.com>
2024-03-11 17:37:25 +0100

    add pass

--- WebAppi/Data/UserStorage.cs
+++ WebAppi/Data/UserStorage.cs
@@ -7,6 +7,7 @@ namespace WebAppi.Data
         public static List<User> Users = new List<User>
         {
             new User {Id = 1, Name = "Mariusz", Password = "mariusz" },
+            new User {Id = 2, Name = "Admin", Password = "admin" },
             new User {Id = 2, Name = "Dariusz", Password = "dariusz" }
         };
     }
